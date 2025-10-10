8c0a6b6a293171692a5a9a6b9aa5011649842c35
muxi <1254301194@qq.com>
2025-10-10 16:32:06 +0800

    Revert "前端系统完善+1"
    This reverts commit 3edf99386a6cba4e71742cebeb6f786682755632.

--- Todo_Front/src/components/List.tsx
+++ Todo_Front/src/components/List.tsx
@@ -1,127 +1,100 @@
 import {Space, Table} from 'antd';
 import type {TableProps} from 'antd';
 import {Button} from 'antd';
-import {useState, useEffect} from 'react';
-import axios from 'axios';
+import {useState} from 'react';
 
 interface DataType {
-    id: string;
-    user: string
+    num: string;
     name: string;
     description: string;
     completed: boolean;
 }
 
-const BaseApi = 'http://localhost:8000'
+const columns: TableProps<DataType>['columns'] = [
+    {
+        title: 'Id',
+        dataIndex: 'num',
+        key: 'num',
+        render: (text) => (
+            <a>{text}</a>
+        ),
+    },
+    {
+        title: 'name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'description',
+        dataIndex: 'description',
+        key: 'description',
+    }, {
+        title: 'completed',
+        dataIndex: 'completed',
+        key: 'completed',
+        render: (completed) => (
+            <Space size="large">
+                {completed ? "✅" : "❌"}
+            </Space>
+        ),
+    },
+    {
+        title: 'Action',
+        key: 'action',
+        render: () => (
+            <Space size="middle">
+                <Button type="primary" shape="circle" onClick={() => {
+                }}>
+                    X
+                </Button>
+                <Button type="primary" shape="circle" onClick={() => {
 
-function GetTodolist(): Promise<DataType[]> {
-    return axios
-        .get(BaseApi + '/get-all')
-        .then(res => {
-            console.log('成功获取list数据')
-            // return res.data
-            return res.data;
-        })
-        .catch(err => {
-            console.log('请求失败：', err)
-        })
-}
+                }}>
+                    ✓
+                </Button>
+                <Button type="primary" shape="round" onClick={() => {
+                    // setData(prevData => {
+                    //         return prevData.Map(
+                    //             if(item.num===num){
+                    //                 return {...item,completed:true};
+                    //             }
+                    //             return item;
+                    //         )
+                    //     }
+                    // )
+                }}>
+                    Change
+                </Button>
+            </Space>
+        ),
+    },
+];
 
-function DeleteTodolist(id: string) {
-    axios
-        .post(BaseApi + '/move', {id})
-        .then(() => GetTodolist())
-        .catch(err => {
-            console.log(err)
-        })
-}
+const [data, setData] = useState<DataType[]>([
+    {
+        "num": "1",
+        "name": "完成作业",
+        "description": "完成数学和英语作业",
+        "completed": false
+    },
+    {
+        "num": "2",
+        "name": "去超市购物",
+        "description": "购买牛奶、面包和水果",
+        "completed": true
+    },
+    {
+        "num": "3",
+        "name": "锻炼身体",
+        "description": "跑步30分钟，做仰卧起坐20个",
+        "completed": false
+    },
+]);
 
-function UpdateTodolist(dat: DataType) {
-    return axios
-        .put(BaseApi + '/update', dat)
-        .catch(err => {
-            console.log(err)
-        })
-}
-
-const List = () => {
-    //数据定义与初始化
-    const [data, setData] = useState<DataType[]>([]);
-    const fetchData = async () => {
-        const result = await GetTodolist();
-        setData(result);
-    };
-    useEffect(() => {
-        fetchData();
-    }, []);
-    //方法定义
-    const handleUpdate = async (x: DataType) => {
-        try {
-            await UpdateTodolist(x)
-            await fetchData();
-        } catch (err) {
-            console.log('更新失败：', err)
-        }
-    }
-    //表单
-    const columns: TableProps<DataType>['columns'] = [
-        {
-            title: 'Index',
-            key: 'index',
-            render: (record) => {
-                return record.index + 1
-            }
-        },
-        {
-            title: 'name',
-            dataIndex: 'name',
-            key: 'name',
-        },
-        {
-            title: 'description',
-            dataIndex: 'description',
-            key: 'description',
-        }, {
-            title: 'completed',
-            dataIndex: 'completed',
-            key: 'completed',
-            render: (completed) => (
-                <Space size="large">
-                    {completed ? "✅" : "❌"}
-                </Space>
-            ),
-        },
-        {
-            title: 'Action',
-            key: 'action',
-            render: (_, record) => (
-                <Space size="middle">
-                    <Button type="primary" shape="circle" onClick={() => {
-                        DeleteTodolist(record.id)
-                    }}>
-                        X
-                    </Button>
-                    <Button type="primary" shape="circle" onClick={() => {
-                        // console.log(record.id)
-                        handleUpdate({...record, completed: true})
-                    }}>
-                        ✓
-                    </Button>
-                    <Button type="primary" shape="round" onClick={() => {
-
-                    }}>
-                        Change
-                    </Button>
-                </Space>
-            ),
-        },
-    ];
-    return (
-        <>
-            <Table<DataType> columns={columns} dataSource={data}/>
-        </>
-    )
-}
-
+const List = () => (
+    <>
+        <Table<DataType> columns={columns} dataSource={data}/>
+    </>
+);
 
 export default List;
